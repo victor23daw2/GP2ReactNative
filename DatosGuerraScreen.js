1f3805ed1a0688210e2a8de7d8c8084f4bee99d1
romasarca <w9tdango98osaw@proton.me>
2024-05-10 17:20:54 +0000

    v.8.1 - Store data from CSV to SQLite, still doesn't work perfectly.

--- DatosGuerraScreen.js
+++ DatosGuerraScreen.js
@@ -7,57 +7,66 @@ import Papa from "papaparse";
 import { Asset } from "expo-asset";
 import { readAsStringAsync } from "expo-file-system";
 
-const LeerDatosCSV = async () => {
+const InsertarDatosCSV = async () => {
+  const db = SQLite.openDatabase("guerrasGP2.db");
   const { localUri: rutaLocal } = await Asset.fromModule(
     require("./assets/UCDP/UcdpPrioConflict_v23_1.csv")
   ).downloadAsync();
   const fitxerCSV = await readAsStringAsync(rutaLocal);
 
-  console.log(fitxerCSV);
+  // Here I create the table.
+  db.transaction((tx) => {
+    try {
+      tx.executeSql(
+        // The name of the table is guerras.
+        "CREATE TABLE IF NOT EXISTS guerrasGP2 (conflict_id INTEGER PRIMARY KEY NOT NULL, location TEXT, side_a TEXT, side_a_id TEXT, side_a_2nd TEXT, side_b TEXT, side_b_id TEXT, side_b_2nd TEXT, incompatibility INTEGER, territory_name TEXT, year INTEGER, intensity_level INTEGER, cumulative_intensity INTEGER, type_of_conflict INTEGER, start_date TEXT, start_prec INTEGER, start_date2 TEXT, start_prec2 INTEGER, ep_end INTEGER, ep_end_date TEXT, ep_end_prec TEXT, gwno_a TEXT, gwno_a_2nd TEXT, gwno_b TEXT, gwno_b_2nd TEXT, gwno_loc TEXT, region TEXT, version INTEGER)"
+      );
+    } catch (error) {
+      console.log(error);
+      throw Error(`Error al crear la taula ${error}.`);
+    }
+  });
 
   Papa.parse(fitxerCSV, {
-    complete: (results) => {
-      this.setState({ data: results.data });
-      console.log("results.data", results.data);
+    worker: true,
+    step: function (row) {
+      // console.log("Fila:", row.data);
+      // console.log("Fila:", row.data[1]);
+
+      db.transaction((tx) => {
+        try {
+          tx.executeSql(
+            `INSERT INTO guerrasGP2 (conflict_id, location, side_a, side_a_id, side_a_2nd, side_b, side_b_id, side_b_2nd, incompatibility, territory_name, year, intensity_level, cumulative_intensity, type_of_conflict, start_date, start_prec, start_date2, start_prec2, ep_end, ep_end_date, ep_end_prec, gwno_a, gwno_a_2nd, gwno_b, gwno_b_2nd, gwno_loc, region, version) VALUES (${row.data[0]}, '${row.data[1]}', '${row.data[2]}', '${row.data[3]}', '${row.data[4]}', '${row.data[5]}', '${row.data[6]}', '${row.data[7]}', ${row.data[8]}, '${row.data[9]}', ${row.data[10]}, ${row.data[11]}, ${row.data[12]}, ${row.data[13]}, '${row.data[14]}', ${row.data[15]}, '${row.data[16]}', ${row.data[17]}, ${row.data[18]}, '${row.data[19]}', '${row.data[20]}', '${row.data[21]}', '${row.data[22]}', '${row.data[23]}', '${row.data[24]}', '${row.data[25]}', '${row.data[26]}', ${row.data[27]})`
+          );
+
+          console.log("Fila:", row.data);
+        } catch (error) {
+          console.log(error);
+          throw Error(`Error a l'insertar dades ${error}`);
+        }
+      });
+    },
+
+    complete: function () {
+      console.log("Dades insertades a SQLite correctament.");
+    },
+
+    error: function (error) {
+      console.error("Error:", error);
     },
   });
 };
 
 export default DatosGuerraScreen = ({ navigation }) => {
-  const db = SQLite.openDatabase("guerras.db");
+  const db = SQLite.openDatabase("guerrasGP2.db");
   // State to show the data on the table.
   let [dadesTaula, setDadesDaula] = useState([]);
-
-  let insertarDades = () => {
-    // Here I open the db.
-    db.transaction((tx) => {
-      try {
-        tx.executeSql(
-          // The name of the table is guerras.
-          "CREATE TABLE IF NOT EXISTS guerras (id INTEGER PRIMARY KEY NOT NULL, done INT, value TEXT);"
-        );
-        // console.log("Taula creada");
-        tx.executeSql("INSERT INTO guerras (done, value) VALUES (0, ?)", [
-          "primer",
-        ]);
-        tx.executeSql("INSERT INTO guerras (done, value) VALUES (1, ?)", [
-          "segon",
-        ]);
-        tx.executeSql("INSERT INTO guerras (done, value) VALUES (2, ?)", [
-          "tercer",
-        ]);
-      } catch (error) {
-        console.log(error);
-        throw Error(`Error a l'insertar dades ${error}`);
-      }
-    });
-  };
 
   let mostrarDades = () => {
     db.transaction((tx) => {
       try {
         console.log("Dades de la taula:");
-        tx.executeSql("SELECT * FROM guerras", [], (_, { rows }) => {
+        tx.executeSql("SELECT * FROM guerrasGP2", [], (_, { rows }) => {
           // Here I have 24 as it's the default size of rows when it's empty, can be checked with: JSON.stringify(rows).length.
           console.log(JSON.stringify(rows).length);
           if (JSON.stringify(rows).length > 24) {
@@ -73,6 +82,28 @@ export default DatosGuerraScreen = ({ navigation }) => {
       } catch (error) {
         console.log(error);
         throw Error(`Error al mostrar dades ${error}`);
+      }
+    });
+  };
+
+  let borrarDades = () => {
+    db.transaction((tx) => {
+      try {
+        // console.log("Borrant dades.");
+        tx.executeSql(
+          "DELETE FROM guerrasGP2",
+          [],
+          (_, { rows }) => console.log(JSON.stringify(rows)),
+          setDadesDaula(
+            (dadesTaula = [
+              ...dadesTaula,
+              "Totes les dades han estat borrades de la taula.",
+            ])
+          )
+        );
+      } catch (error) {
+        console.log(error);
+        throw Error(`Error al borrar dades ${error}`);
       }
     });
   };
@@ -93,17 +124,11 @@ export default DatosGuerraScreen = ({ navigation }) => {
       </View>
 
       <View>
-        <Button title="Insertar dades" onPress={insertarDades}>
-          Insertar dades
-        </Button>
+        <Button title="Insertar dades CSV" onPress={InsertarDatosCSV}></Button>
 
-        <Button title="Mostrar dades" onPress={mostrarDades}>
-          Mostrar dades
-        </Button>
+        <Button title="Mostrar dades" onPress={mostrarDades}></Button>
 
-        <Button title="Llegir CSV" onPress={LeerDatosCSV}>
-          Llegir dades CSV
-        </Button>
+        <Button title="Borrar taula" onPress={borrarDades}></Button>
       </View>
 
       <Button
